03dcad0124fd9caafad8ea73df853edac0b5a765
nodap22 <sanha0046@naver.com>
2024-01-25 20:21:20 +0900

    Committing local changes before pull

--- dashboard/src/main/java/blog/domain/DashBoard.java
+++ dashboard/src/main/java/blog/domain/DashBoard.java
@@ -2,25 +2,21 @@ package blog.domain;
 
 import javax.persistence.*;
 import java.util.List;
-import java.util.Date;
 import lombok.Data;
-import java.time.LocalDate;
 
-
-//<<< EDA / CQRS
 @Entity
-@Table(name="DashBoard_table")
+@Table(name = "DashBoard_table")
 @Data
 public class DashBoard {
 
         @Id
-        //@GeneratedValue(strategy=GenerationType.AUTO)
+        @GeneratedValue(strategy = GenerationType.AUTO)
         private Long id;
         private Long postId;
         private String postTitle;
         private String postContent;
         private String postNickname;
-        private List&lt;String&gt; commentList;
-
-
+        // 안치윤 : 필드 수정 및 어노테이션 추가
+        @ElementCollection
+        private List<Comment> commentList;
 }
